70f982125344f044954bc80d9d0607115a5d4cbe
gih1214 <gih1214@hanmail.net>
2022-04-29 17:46:25 +0900

    initial commit

---
+++ src/test/java/site/metacoding/weathertour/WeatherTourApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.weathertour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
